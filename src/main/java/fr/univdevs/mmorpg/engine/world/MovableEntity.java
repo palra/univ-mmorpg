4e8b03d845d05ac40a301f2fed3ffd42154e06cd
drattak <vl.emile@gmail.com>
2015-05-19 13:35:19 +0200

    MovableEntity Interface

---
+++ src/main/java/fr/univdevs/mmorpg/engine/world/MovableEntity.java
@@ -0,0 +1,9 @@
+package fr.univdevs.mmorpg.engine.world;
+
+/**
+ * Created by drattak on 19/05/15.
+ */
+public interface MovableEntity extends Entity {
+    public void setX(int x);
+    public void setY(int y);
+}
