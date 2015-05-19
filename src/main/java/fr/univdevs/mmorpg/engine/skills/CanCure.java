57a96bb634b45c402fa8a6e112ddafc07a3314bf
drattak <vl.emile@gmail.com>
2015-05-19 14:30:25 +0200

    interfaces from skill packages

--- src/main/java/fr/univdevs/mmorpg/engine/skills/CanCure.java
+++ src/main/java/fr/univdevs/mmorpg/engine/skills/CanCure.java
@@ -4,4 +4,5 @@ package fr.univdevs.mmorpg.engine.skills;
  * Created by drattak on 19/05/15.
  */
 public interface CanCure {
+    public void cure(Character c, String cureName);
 }
--- src/main/java/fr/univdevs/mmorpg/engine/skills/CanSpell.java
+++ src/main/java/fr/univdevs/mmorpg/engine/skills/CanSpell.java
@@ -3,5 +3,6 @@ package fr.univdevs.mmorpg.engine.skills;
 /**
  * Created by drattak on 19/05/15.
  */
-public interface CanDefend {
+public interface CanSpell {
+    public void Spell(Character c, String spellName);
 }
