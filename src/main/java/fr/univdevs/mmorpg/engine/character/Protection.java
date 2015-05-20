595a573ffe4c11396abe98df6d69340c24644904
drattak <vl.emile@gmail.com>
2015-05-20 10:19:26 +0200

    new abstract class Protection extends Item

---
+++ src/main/java/fr/univdevs/mmorpg/engine/character/Protection.java
@@ -0,0 +1,18 @@
+package fr.univdevs.mmorpg.engine.character;
+
+/**
+ * Created by drattak on 20/05/15.
+ */
+public abstract class Protection extends Item {
+    private double robustness;
+
+    public Protection(String protectionName, String protectionCategory, int protectionCost, int protectionWeight,double protectionRobustness){
+        super(protectionName,protectionCategory,protectionCost, protectionWeight);
+        this.robustness = protectionRobustness;
+    }
+
+    public double getRobustness(){
+        return this.robustness;
+    }
+
+}
