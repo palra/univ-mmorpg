bde904690fc68cd173595b208f243c0e0545b7c1
drattak <vl.emile@gmail.com>
2015-05-24 21:00:01 +0200

    added action package added Action class in action package

---
+++ src/main/java/fr/univdevs/mmorpg/engine/action/Action.java
@@ -0,0 +1,42 @@
+package fr.univdevs.mmorpg.engine.action;
+
+import fr.univdevs.mmorpg.engine.character.*;
+import fr.univdevs.mmorpg.engine.character.Character;
+
+/**
+ * Public class Action
+ * An action is any thing that can do the character
+ * The action is done by a subject to a target
+ */
+public abstract class Action {
+    private Character subject;
+    private Character target;
+
+    /**
+     * Action constructor
+     *
+     * @param chosenSubject the Character who execute the action
+     * @param chosenTarget  the Character targeted
+     */
+    public Action(Character chosenSubject, Character chosenTarget) {
+        this.subject = chosenSubject;
+        this.target = chosenTarget;
+    }
+
+    /**
+     * Public method to return the target of the Action
+     *
+     * @return the target
+     */
+    public Character getTarget() {
+        return this.target;
+    }
+
+    /**
+     * Abstract method to execute the action
+     * Will describe the impact of the action on the targert
+     *
+     * @return the Action executed
+     */
+    public abstract Action execute();
+}
