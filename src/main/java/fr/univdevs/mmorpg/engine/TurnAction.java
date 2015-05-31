e83a060a93406a0a2ce620a41a9297d5034bae15
drattak <vl.emile@gmail.com>
2015-05-31 19:29:45 +0200

    new class Turn Action

---
+++ src/main/java/fr/univdevs/mmorpg/engine/TurnAction.java
@@ -0,0 +1,11 @@
+package fr.univdevs.mmorpg.engine;
+
+/**
+ * class TurnAction
+ * Describe a Game Turn
+ * Every player does an action
+ */
+public class TurnAction {
+    public TurnAction() {
+    }
+}
