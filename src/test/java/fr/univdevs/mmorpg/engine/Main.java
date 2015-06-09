db6e341e98b77957b211782d22bb10c5cddcd2dd
drattak <vl.emile@gmail.com>
2015-06-09 11:58:54 +0200

    completed maintest

--- src/test/java/fr/univdevs/mmorpg/engine/Main.java
+++ src/test/java/fr/univdevs/mmorpg/engine/Main.java
@@ -2,35 +2,43 @@ package fr.univdevs.mmorpg.engine;
 
 import fr.univdevs.mmorpg.engine.character.*;
 import fr.univdevs.mmorpg.engine.character.Character;
+import fr.univdevs.mmorpg.engine.world.World;
 import fr.univdevs.mmorpg.game.character.Healer;
 import fr.univdevs.mmorpg.game.character.Warrior;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Scanner;
 
 /**
  * Created by drattak on 08/06/15.
  */
 public class Main {
+    private static HashMap<String, Player> players = new HashMap<String, Player>();
     public static void main(String[] args) {
         init();
+        fight(players);
     }
 
     public static void init() {
         System.out.println("Combien de joueurs ?");
         int number = new Scanner(System.in).nextInt();
         for (int i = 0; i < number; i++) {
-            System.out.println("JOUEUR " + i + 1);
-            characterInit();
+            System.out.println("JOUEUR " + (i + 1));
+            playerInit();
         }
     }
 
-    public static Character characterInit() {
-        System.out.println("Entrez le nom : ");
+
+    public static Player playerInit() {
+        System.out.println("Entrez le nom du joueur : ");
+        String playerName = new Scanner(System.in).nextLine();
+        System.out.println("Entrez le nom du personnage : ");
         String name = new Scanner(System.in).nextLine();
         System.out.println("Quelle categorie ?\n1.Warrior\n2. Healer");
         int category = new Scanner(System.in).nextInt();
-        return determineCharacter(category, name);
+        return players.put(playerName, new Player(playerName, determineCharacter(category, name)));
     }
 
     public static Character determineCharacter(int category, String name) {
@@ -42,5 +50,9 @@ public class Main {
         }
         return null;
     }
+
+    public static void fight(HashMap<String, Player> players) {
+    }
+
 }
 
