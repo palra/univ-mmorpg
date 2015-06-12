2296104d42326785316e9902531b9f70109045d8
Loïc Payol <loicpayol@gmail.com>
2015-06-12 11:19:31 +0200

    Fixed random32 / 64

--- src/main/java/fr/univdevs/util/Strings.java
+++ src/main/java/fr/univdevs/util/Strings.java
@@ -45,7 +45,7 @@ public class Strings {
      * @return A random hexadecimal string
      */
     public static String random32() {
-        return Integer.toHexString(random.nextInt());
+        return fillLeftIn(Integer.toHexString(random.nextInt()), 8, '0');
     }
 
     /**
@@ -54,6 +54,27 @@ public class Strings {
      * @return A random hexadecimal string
      */
     public static String random64() {
-        return Long.toHexString(random.nextLong());
+        return fillLeftIn(Long.toHexString(random.nextLong()), 16, '0');
+    }
+
+    /**
+     * When in is smaller than the given length, left fill with `fill` character.
+     *
+     * @param in     The input string
+     * @param length The wanted length of the string
+     * @param fill   The character to fill with
+     * @return The filled string
+     */
+    public static String fillLeftIn(final String in, int length, char fill) {
+        int diff = length - in.length();
+        if (diff <= 0)
+            return in;
+
+        String out = "";
+        for (int i = 0; i < diff; i++) {
+            out += fill;
+        }
+
+        return out + in;
     }
 }
